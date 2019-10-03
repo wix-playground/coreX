7295590db6dea5babe783e53b848cb6ecde78296
Rostyslav <rostyslavp@wix.com>
2019-10-03 12:16:42 +0300

    update mocks

--- lib/data-layer/mocks.dart
+++ lib/data-layer/mocks.dart
@@ -5,7 +5,10 @@ const JSONMocks = {
     "uiSchema": {
       "order": [
         "id1",
-        "id2"
+        "id2",
+        "id3",
+        "id4",
+        "id5",
       ],
       "components": {
         "id1": {
@@ -21,6 +24,27 @@ const JSONMocks = {
           "handlers": [
             "alert"
           ]
+        },
+        "id3": {
+          "id": "id3",
+          "type": "link",
+          "value": {
+            "src": "www.wix.com",
+          }
+        },
+        "id4": {
+          "id": "id4",
+          "type": "text",
+          "value": {
+            "text": "Wix is Cool!",
+          }
+        },
+        "id5": {
+          "id": "id5",
+          "type": "image",
+          "value": {
+            "src": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png",
+          }
         }
       }
     }
