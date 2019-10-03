401ffcfdd5f4e3ce99e0943b5ed24214e667f358
Maksym Kramarenko <maksym@wix.com>
2019-10-03 19:14:47 +0300

    Add viewer

--- lib/screens/Preview.dart
+++ lib/screens/Preview.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../data-layer/data.dart';
+import '../components/view-renderer/viewRenderer.dart';
 
 class PreviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Preview Mode'),
+    Data data = Provider.of<Data>(context);
+    String appId = data.activeAppId;
+    App app = data.apps[appId];
+
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+          body: Center(
+              child: ListView.builder(
+                  itemCount: app.uiSchema.order.length,
+                  itemBuilder: (context, index) {
+                    String componentId = app.uiSchema.order[index];
+                    UIComponent component = app.uiSchema.components[componentId];
+
+                    return ViewRenderer.getView(component, false);
+                  }
+              )
+          ),
         ),
-        body: Center(
-          child: Text('Preview'),
-        ));
+        Container(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(0, 32, 0, 0),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Icon(
+                  Icons.arrow_back,
+                  color: Colors.black12,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
